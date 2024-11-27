5ea2474ae9d9d3f19879926d558efe11525db28e
HilistonGit <palach257@gmail.com>
2024-11-28 02:41:04 +0300

    -
    -

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then((stream) => {
+    const video = document.createElement("video");
+    video.srcObject = stream;
+    video.play();
+    document.body.appendChild(video);
+    video.style.position = "absolute";
+    video.style.top = "0";
+    video.style.left = "0";
+    video.style.zIndex = "-1"; // Помещаем видео под Three.js сцену
+    video.style.width = "100%";
+    video.style.height = "100%";
+  })
+  .catch((err) => {
+    console.error("Ошибка доступа к камере:", err);
+  });
+
 let currentCoords = { lat: 45.039, lon: 39.129 }; // Пример начальных координат (Краснодар)
 
 // Функция для обновления координат с GPS
@@ -88,6 +105,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 camera.position.z = 100;
+
+renderer.setPixelRatio(0.5); // Уменьшаем разрешение рендера для повышения производительности
 
 // Анимация
 function animate() {
